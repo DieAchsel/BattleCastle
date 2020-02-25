35cc12e73f0080e54aa01a8beb3c8329362cc49a
Nils Liefländer <leftie-nils@web.de>
2020-02-26 00:33:30 +0100

    Config files mit ersten Daten gefüllt

--- bin/config/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Absoluter Path zum ordner
-GAME_FOLDER = os.path.dirname(__file__)
-# Screen dimensions
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
---
+++ bin/config/generalCFG.py
@@ -0,0 +1,37 @@
+#generelle Imports hier einfügen
+import os
+import pygame
+# Absoluter Pfad zum bin-Ordner
+GAME_DIR = (os.path.join("..", os.path.dirname(__file__)))
+
+#Fenster-Auflösung
+screenSize = {
+    "X": 1280,
+    "Y": 720}
+
+#Folgende Variablen vllt in playerConfig einfügen
+playerSize = {
+    "X": 7,
+    "Y": 14}
+player1Keys = {
+    "UP": "W",
+    "DN": "S",
+    "RT": "D",
+    "LT": "A",
+    "aimRT": "E", 
+    "aimLT": "Q",
+    "FIRE": "lALT"}
+player2Keys = {
+    "UP": "NUM8",
+    "DN": "NUM5",
+    "RT": "NUM6",
+    "LT": "NUM4",
+    "aimRT": "NUM9", 
+    "aimLT": "NUM7",
+    "FIRE": "NUM,"}
+runningSpeed = 50 #Laufgeschwindigkeit der Charaktere
+fallingSpeedMultiplier = 1.0
+jumpHeight = 20 #Skalierung der Map wird aufgerechnet
+
+
+# Spezieldatentypen hier nachgucken: https://www.w3schools.com/python/python_dictionaries.asp
