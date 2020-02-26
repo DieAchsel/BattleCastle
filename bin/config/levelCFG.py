c73d3384bbe480c5455a8214d9e84cfc14522e18
Nils Liefländer <leftie-nils@web.de>
2020-02-27 00:25:03 +0100

    Cleanp und Kommentare

--- bin/config/levelCFG.py
+++ bin/config/levelCFG.py
@@ -1,7 +1,8 @@
-#Parameter und Variablen zu leveln
-#es handelt sich hierbei um dynamischen Content
+#Konfiguration für den Level-Bereich. 
+#Dieser beinhaltet Level, Levelmanager und Tile
+#[kleine Info] es handelt sich hierbei teils um dynamischen Content
 from bin.config.generalCFG import SCREEN_SIZE
-DEFAULT_LAYER_ID = 1 #Die Standardschicht, auf die ein level gezeichnet werden soll
+DEFAULT_LAYER_ID = 1 #Die Standardschicht, auf die ein Tile gezeichnet werden soll (0 ist für den Hintergrund)
 DEFAULT_TILE_SIZE = {
     "X": 64,
     "Y": 64}
