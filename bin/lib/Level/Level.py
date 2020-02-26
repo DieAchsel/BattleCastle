441c3dc8c040151cdd8be7e42ce66acd4e1f8ba8
Nils Liefländer <leftie-nils@web.de>
2020-02-26 17:00:06 +0100

    cleanup

--- bin/lib/Level/Level.py
+++ bin/lib/Level/Level.py
@@ -7,8 +7,10 @@ class Level:
     name = ""
     id
     tilemap = []
-    texture = {# hier komme ich etwas durcheinander mit den Datentypen: textureSeq soll eine Lise mit texturen beinhalten
+
     #texture selber soll wie ein struct (c++) agieren und nur diese 3 subtypen haben.
+    #hier komme ich etwas durcheinander mit den Datentypen: textureSeq soll eine Lise mit texturen beinhalten
+    texture = {
         "textureSeq": [pygame.image],
         "id": 0,
         "Neighbors": [[0,0,0],
@@ -25,7 +27,6 @@ class Level:
     def getTile(self, pos = {"X": 0, "Y": 0}):
         if(pos.X < self.tilemap.len & pos.Y < self.tilemap[pos.X].len):
             return self.tilemap[pos.X[pos.Y]]
-
 
     def setTile(self, pos = {"X": 0, "Y": 0}, type = 1):
         if(pos.X < self.tilemap.len & pos.Y < self.tilemap[pos.X].len):
