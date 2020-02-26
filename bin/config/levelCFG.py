a38ba56afc4d5476f3288c6703efe3d41b469c71
Nils Liefländer <leftie-nils@web.de>
2020-02-26 23:43:53 +0100

    weitere Config-Parameter ergänzt

--- bin/config/levelCFG.py
+++ bin/config/levelCFG.py
@@ -1,9 +1,16 @@
 #Parameter und Variablen zu leveln
 #es handelt sich hierbei um dynamischen Content
-from bin.config.generalCFG import screenSize
-gridSize = {
+from bin.config.generalCFG import SCREEN_SIZE
+DEFAULT_LAYER_ID = 1 #Die Standardschicht, auf die ein level gezeichnet werden soll
+DEFAULT_TILE_SIZE = {
+    "X": 64,
+    "Y": 64}
+DEFAULT_GRID_SIZE = {
     "X": 32,
     "Y": 32}
-arenaSize = {
-    "X": screenSize["X"]*1.0,
-    "Y": screenSize["Y"]* 0.95}
+ARENA_SIZE = {
+    "X": SCREEN_SIZE["X"]*1.0,
+    "Y": SCREEN_SIZE["Y"]* 0.95}
+
+DEFAULT_DMG_ON_COLLISION = 10
+DEFAULT_DMG_OVER_TIME = 4
