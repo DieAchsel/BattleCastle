48441318d67916de19f5ea87fdb096ab06ae270d
Nils Liefländer <leftie-nils@web.de>
2020-02-29 02:31:46 +0100

    cleanup uund einige Anpassungen / erweiterungen

--- bin/config/levelCFG.py
+++ bin/config/levelCFG.py
@@ -1,25 +1,26 @@
 #Konfiguration für den Level-Bereich. 
-#Dieser beinhaltet Level, Levelmanager und Tile
+#Diese Datei beinhaltet Parameter für Level, Levelmanager und Tile
 #[kleine Info] es handelt sich hierbei teils um dynamischen Content
-import re
-import string
+import re, os, glob, string
 from pygame.rect import Rect
-import os
-import glob
 from bin.config.generalCFG import SCREEN_SIZE, GAME_DIR
 
-LEVEL_DIR = os.path.join(GAME_DIR, 'lvl', '')
 
-DEFAULT_LAYER_ID = 1 #Die Standardschicht, auf die ein Tile gezeichnet werden soll (0 ist für den Hintergrund)
+#-----------------------------Arena-Parameter-------------------------------
+#Standardschicht, auf die die Arena gezeichnet wird
+DEFAULT_LAYER_ID = 1 #(0 ist für den Hintergrund reserviert)
 
 #Größe und Position der Arena
-#Die Arena-Area wird mit relativen Größen (in beziehung zu SCREEN_SIZE in generalCFG.py) positioniert
-#Bei Breiten unter 100% wird die PlayArea zentriert
-#Bei Höhen unter 100% erhält die StatusArea ( über statusAreaCFG.py in Beziehung mit ARENA_HEIGHT_PERCENT)
-ARENA_HEIGHT_PERCENT = 80 #der Rest geht an den Statusbereich über
-ARENA_WIDTH_PERCENT = 100
+    #Die Arena-Area wird mit relativen Größen (in beziehung zu SCREEN_SIZE in generalCFG.py) positioniert
+        #Bei Breiten unter 100% wird die PlayArea zentriert
+        #Bei Höhen unter 100% erhält die StatusArea ( über statusAreaCFG.py in Beziehung mit ARENA_HEIGHT_PERCENT) einen eigenen Bereich
+        #Bei Höhen von 100% erhält die StatusArea ( über statusAreaCFG.py in Beziehung mit ARENA_HEIGHT_PERCENT) einen Bereich im nächsten Layer und der statusArea-Hintergrund wird deaktiviert
+ARENA_HEIGHT_PERCENT = 80 #Bei Größen unter 100% wird der freie Bereich (unten angefügt) für die StatusArea genutzt
+ARENA_WIDTH_PERCENT = 100 #Bei Größen unter 100% wird Arena auf der Display-Surface zentriert
+#Position auf der Display-Surface
 ARENA_X_POS = round(((SCREEN_SIZE["X"]/100) * (100 - ARENA_WIDTH_PERCENT)),0)
 ARENA_Y_POS = 0
+#Rect mit sämtlichen Informationen (in Pixel übersetzt) für Programm-Zugriff
 ARENA_AREA = Rect(ARENA_X_POS, ARENA_Y_POS, round((SCREEN_SIZE["X"] * (ARENA_WIDTH_PERCENT / 100)),0), round((SCREEN_SIZE["Y"] * (ARENA_HEIGHT_PERCENT / 100)),0))
 
 #DEPRECATED--
@@ -30,10 +31,31 @@ ARENA_SIZE = {
 ARENA_POSITION = (0,0)
 #--DEPRECATED
 
+#-----------------------------Tile-Parameter--------------------------------
+#Standard-tile-typ, wenn kein tile-typ zugeordnet werden kann (sollte nicht passieren)
+DEFAULT_TILE_ID = 0   #0 = unsichtbar(wird beim level.build() in die spritegroup unusedTiles verschoben)
+
+TILE_ID_RANGE_MAP = {
+    "harmful_collidable": {"first": 51, "last": 100},
+    "collidable": {"first": 1, "last": 50},
+    "empty": 0,
+    "uncollidable": {"first": -50, "last": -1},
+    "harmful_uncollidable": {"first": -100, "last": -51}
+}
+
+#Standard-Texture-Set für Tile
+DEFAULT_TEXTURE_SET_PATH = os.path.join(GAME_DIR, "gfx", "tiles", "default")
+
+#Harming-Tiles:
 DEFAULT_DMG_ON_COLLISION = 10
 DEFAULT_DMG_OVER_TIME = 4
 
-#------------------------------.lvl-Handling-------------------------------
+#-----------------------allgemeines Level-Verhalten-------------------------
+#Ersetzt beim Levelwechsel die surfaceObjekte im Raster durch ihre TypeID. 
+#spart RAM, jedoch muss das Level beim erneuten Laden neu gebaut (build()) werden
+IS_UNBUILDING_ON_UNLOAD = True
+
+#------------------------------Datei-Handling-------------------------------
 #RegEx Ausdrücke zum Parsen der .lvl Dateien
 #jede zu lesende Variable wird hier registriert
 #wenn die Ausdrücke in der Datei nicht gefunden wurden oder unzulässig sind, werden Default-Werte verwendet
@@ -44,17 +66,10 @@ DATA_CONDITIONS = {
     "playerStartPos": "^playerStartPos=(\(\d+;\d+\))+$",
     "grid": "^GRID=(([\d+|-\d+]+)+)$" #hier stimmt was nicht mehr, ich hab nur ein semikolon entfernt. zudem hat die regEx negative werte glaub ich übersprungen
 }
+LEVEL_DIR = os.path.join(GAME_DIR, 'lvl', '')
 
-#
-#test = open("C:/Users/lefti/Documents/Software-Projekte/PYTHON/BattleCastle/bin/lvl/000/TestCastle.lvl")
-#for i in test:
-#    i = i.replace(" ", "")
-#    m = re.search('^GRID=(([\d+|\-\d+]);)+$' , i)
-#    print(m)
-
-
-
-#.lvl Defaultwerte:
+#-------------------------------Default Level--------------------------------
+#Werte, die genutzt werden, wenn es Probleme mit dem Laden der .lvl Dateien gibt
 DEFAULT_TEXTURE_PATH = os.path.join(GAME_DIR, "gfx", "tiles", "00")
 DEFAULT_BG_PATH = os.path.join(GAME_DIR, "gfx", "bg", "00", "Full.png")
 DEFAULT_DIFFICULTY = 0
