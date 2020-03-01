753a315fc751ebf43d0c4892ea9b66cb02bf373b
Nils Liefländer <leftie-nils@web.de>
2020-03-01 20:10:27 +0100

    Debugging implementiert

--- bin/config/generalCFG.py
+++ bin/config/generalCFG.py
@@ -1,10 +1,35 @@
 #generelle Imports hier einfügen
 import os
 import pygame
+
+NULL_TYPE = 0
+
+
+DEBUG_ENABLED = True
+DEBUG_LEVEL = 9
+#Wenn DEBUG_ENABLED, dann gib Debuggingmeldungen aus, je nach lvl mit angepasstem Detail
+#wie detailliert sollen die Meldungen sein. bei DEBUG_LEVEL >= 9 wird nach möglichkeit jede Daten-Transaktion in der Konsole ausgegeben
+#Debugging-Funktion:
+def DEBUG(msg = "DEBUG Ohne Nachricht", debugLevel = 0, ObjectToPrint = NULL_TYPE):
+    if(DEBUG_ENABLED):
+        if(debugLevel >= DEBUG_LEVEL):
+            tabs = ""
+            for x in range(debugLevel):
+                #Rücke Meldungen in tieferem Layer ein
+                tabs += "    "
+            output = "DEBUGGING: " + tabs + msg
+
+            if(ObjectToPrint != NULL_TYPE): 
+                objectOutput = "\nEnthaltenes Objekt:\n" + ObjectToPrint
+                output += objectOutput
+            print(output)
+#Wenn DEBUG_ENABLED, gib diese Nachricht aus:
+DEBUG("********************Debugging Aktiv********************\nDebug-Level = " + DEBUG_LEVEL + "\n\n\n")
+
 # Absoluter Pfad zum bin-Ordner
 GAME_DIR = (os.path.join(os.path.dirname(os.path.dirname(__file__))))
 
-NULL_TYPE = 0
+
 #Fenster-Auflösung
 SCREEN_SIZE = {
     #"X": 1280,
