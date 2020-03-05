dfd1d49a7f4abb6119560fc7efd6f03b9d94f1e7
Nils Liefländer <leftie-nils@web.de>
2020-03-05 20:29:17 +0100

    available IMG Format regex eingefügt

--- bin/config/generalCFG.py
+++ bin/config/generalCFG.py
@@ -5,7 +5,7 @@ import pygame
 
 VERSION = "0.1.1"
 NULL_TYPE = 0
-
+AVAILABLE_IMG_FORMAT_REGEX = "[png|gif|jpg|bmp|pcx|tga|tif|lbm|pbm|pgm|ppm|xpm]"
 # Absoluter Pfad zum bin-Ordner
 GAME_DIR = (os.path.join(os.path.dirname(os.path.dirname(__file__))))
 DEBUG_ENABLED = True
@@ -41,7 +41,6 @@ SCREEN_SIZE = {
 }
 
 FPS = 60
-
 
 #-------------------------------------Render-Optionen--------------------------------------------------
 
