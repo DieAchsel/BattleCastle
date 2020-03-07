045d203a319c9e431d359e3da20fa6b3198fd376
Nils Liefländer <leftie-nils@web.de>
2020-03-07 16:52:30 +0100

    DEBUG-Funktion um einstellbares delay und userinput erweitert

--- bin/config/generalCFG.py
+++ bin/config/generalCFG.py
@@ -1,6 +1,7 @@
 #generelle Imports hier einfügen
 import os
 import pygame
+import time
 
 
 VERSION = "0.1.1"
@@ -13,24 +14,47 @@ else:
 # Absoluter Pfad zum bin-Ordner
 GAME_DIR = (os.path.join(os.path.dirname(os.path.dirname(__file__))))
 DEBUG_ENABLED = True
-DEBUG_LEVEL = 7
+DEBUG_LEVEL = 12
+DELAY_ON_DEBUG = 0 #gibt ein Delay in s (darf float sein) an, wie lang nach jeder DEBUGGING-Nachricht gewartet werden soll.
+ON_DEBUG_USER_CONTINUES = True #wenn ein kontinueKey (pygame.event) angegeben ist, wird bis zur Eingabe von Enter gewartet.
+DEBUG_TAB = "    "
 #Wenn DEBUG_ENABLED, dann gib Debuggingmeldungen aus, je nach lvl mit angepasstem Detail
-#wie detailliert sollen die Meldungen sein. bei DEBUG_LEVEL >= 9 wird nach möglichkeit jede Daten-Transaktion in der Konsole ausgegeben
+#wie detailliert sollen die Meldungen sein. bei DEBUG_LEVEL >= 12 wird je nach integrierung jede Daten-Transaktion in der Konsole ausgegeben
 #Debugging-Funktion:
 def DEBUG(msg = "Meldung ohne Inhalt", debugLevel = 0, ObjectToPrint = NULL_TYPE):
     if(DEBUG_ENABLED):
         if(debugLevel <= DEBUG_LEVEL):
-            tabs = ""
+            spacing = ""
+            
+            output_prefix = "DEBUGGING: "
             for x in range(debugLevel):
                 #Rücke Meldungen in tieferem Layer ein
-                tabs += "    "
-            output = "DEBUGGING: " + tabs + msg
+                spacing += DEBUG_TAB
+            output = output_prefix + spacing + msg
             print(output)
             if(ObjectToPrint != NULL_TYPE): 
-                output = "DEBUGGING:     " + tabs + str(ObjectToPrint)
+                output = output_prefix + spacing + DEBUG_TAB + str(ObjectToPrint)
                 print(output)
+            if(ON_DEBUG_USER_CONTINUES):
+                output = output_prefix + spacing + DEBUG_TAB + "Warte auf Bestätigung"
+                print(output)
+                input()
+            if(DELAY_ON_DEBUG > 0):
+                time.sleep(DELAY_ON_DEBUG)
 #Wenn DEBUG_ENABLED, gib diese Nachricht aus:
-
+def DEBUG_OBJECT_PRINTER(sequence = NULL_TYPE):
+    if(sequence == NULL_TYPE):
+        return ""
+    else:
+        if(type(sequence) == str):
+            return str(sequence)
+        else:
+            msg = ""
+            if(hasattr(sequence, '__iter__')):
+                for element in sequence:
+                    msg += DEBUG_TAB + DEBUG_OBJECT_PRINTER(element) + os.linesep 
+            else:
+                return str(sequence)
 
 ICON_PATH = os.path.join(GAME_DIR, "gfx", "icon", "castle.png")
 #Fenster-Auflösung
