5a8cd790c24f125a301940c0357cc094a6715e42
Nils Liefländer <leftie-nils@web.de>
2020-03-09 19:45:36 +0100

    Debugging um zeitangabe und FileLogging erweitert

--- bin/config/generalCFG.py
+++ bin/config/generalCFG.py
@@ -4,7 +4,8 @@ import pygame
 import time
 
 
-VERSION = "0.1.1"
+
+VERSION = "0.1.2"
 NULL_TYPE = None
 ANIMATION_INTERVAL = 30 #jedes 30te Update wird die bildsequenz iteriert
 if(pygame.image.get_extended()):
@@ -14,48 +15,42 @@ else:
 # Absoluter Pfad zum bin-Ordner
 GAME_DIR = (os.path.join(os.path.dirname(os.path.dirname(__file__))))
 DEBUG_ENABLED = True
-DEBUG_LEVEL = 12
+DEBUG_LEVEL = 13
 DELAY_ON_DEBUG = 0 #gibt ein Delay in s (darf float sein) an, wie lang nach jeder DEBUGGING-Nachricht gewartet werden soll.
-ON_DEBUG_USER_CONTINUES = True #wenn ein kontinueKey (pygame.event) angegeben ist, wird bis zur Eingabe von Enter gewartet.
-DEBUG_TAB = "    "
+ON_DEBUG_USER_CONTINUES = False #wenn ein kontinueKey (pygame.event) angegeben ist, wird bis zur Eingabe von Enter gewartet.
+
+LOG_FILE = True
 #Wenn DEBUG_ENABLED, dann gib Debuggingmeldungen aus, je nach lvl mit angepasstem Detail
 #wie detailliert sollen die Meldungen sein. bei DEBUG_LEVEL >= 12 wird je nach integrierung jede Daten-Transaktion in der Konsole ausgegeben
 #Debugging-Funktion:
+logOpened = bool
+logFile = open(os.path.join(GAME_DIR, "DEBUG.log"), 'a')
 def DEBUG(msg = "Meldung ohne Inhalt", debugLevel = 0, ObjectToPrint = NULL_TYPE):
-    if(DEBUG_ENABLED):
-        if(debugLevel <= DEBUG_LEVEL):
-            spacing = ""
-            
-            output_prefix = "DEBUGGING: "
-            for x in range(debugLevel):
-                #Rücke Meldungen in tieferem Layer ein
-                spacing += DEBUG_TAB
-            output = output_prefix + spacing + msg
-            print(output)
-            if(ObjectToPrint != NULL_TYPE): 
-                output = output_prefix + spacing + DEBUG_TAB + str(ObjectToPrint)
-                print(output)
-            if(ON_DEBUG_USER_CONTINUES):
-                output = output_prefix + spacing + DEBUG_TAB + "Warte auf Bestätigung"
-                print(output)
-                input()
+    from bin.config.generalCFG import logOpened, logFile
+    if(DEBUG_ENABLED):  
+        spacing = ""  
+        tabs = "    " 
+        output_prefix = "DEBUGGING: " + time.strftime("%H:%M:%S") + "   "
+        output = []    
+        for x in range(debugLevel): #Rücke Meldungen in tieferem Layer ein
+            spacing += tabs
+        output.append(output_prefix + spacing + msg)
+        if(ObjectToPrint != NULL_TYPE): 
+            output.append(output_prefix + spacing + tabs + str(ObjectToPrint))
+        if(DELAY_ON_DEBUG > 0):
+            output.append(output_prefix + spacing + tabs + "Warte auf Bestätigung")
+        if(debugLevel <= DEBUG_LEVEL):     
             if(DELAY_ON_DEBUG > 0):
                 time.sleep(DELAY_ON_DEBUG)
-#Wenn DEBUG_ENABLED, gib diese Nachricht aus:
-def DEBUG_OBJECT_PRINTER(sequence = NULL_TYPE):
-    if(sequence == NULL_TYPE):
-        return ""
-    else:
-        if(type(sequence) == str):
-            return str(sequence)
-        else:
-            msg = ""
-            if(hasattr(sequence, '__iter__')):
-                for element in sequence:
-                    msg += DEBUG_TAB + DEBUG_OBJECT_PRINTER(element) + os.linesep 
-            else:
-                return str(sequence)
+            if(ON_DEBUG_USER_CONTINUES):
+                output.append(output_prefix + spacing + tabs + "Nutzer-Eingabe: '" + input() + "'")
+            for x in output:
+                print(x)
+                if(LOG_FILE):
+                    logFile.write((x + "\n"))
+                    logFile.flush()
 
+                    
 ICON_PATH = os.path.join(GAME_DIR, "gfx", "icon", "castle.png")
 #Fenster-Auflösung
 SCREEN_SIZE = {
